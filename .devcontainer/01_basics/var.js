e9207fe088c869901b34eaddb28beddb1b55af87
Shruti Munale <95207262+Shruti805@users.noreply.github.com>
2026-09-02 11:15:54 +0000

    var and const done

---
+++ .devcontainer/01_basics/var.js
@@ -0,0 +1,13 @@
+let user = "shruti"
+var user1 = "Munale" // Mostly we will use let and const we will not use var bcs it will create scope issue
+const accountBal = 500 // by using const we cannot change value
+let user2; //undefined
+user = "Shruti_Munale"
+user1 = "barbase"
+console.log(user);
+// accountBal = 1500 // it will give error
+// console.log(accountBal);
+console.log(user1);
+console.table([user, user1, accountBal])
+
+
